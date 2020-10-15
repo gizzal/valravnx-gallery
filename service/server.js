19a19616387f44d238ebc251c591a6477623d825
home No To ri <soy_solamente@hotmail.com>
2020-10-16 00:52:57 +0300

    add files

--- service/server.js
+++ service/server.js
@@ -2,48 +2,22 @@ const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
+const blobService = require('feathers-blob');
 
-class MessageService {
-  constructor() {
-    this.messages = [];
-  }
-
-  async find () {
-    return this.messages;
-  }
-
-  async create (data) {
-    const message = {
-      id: this.messages.length,
-      text: data.text
-    }
-
-    this.messages.push(message);
-
-    return message;
-  }
-}
+const fs = require('fs-blob-store');
+const blobStorage = fs(__dirname + '/uploads/pictures');
 
 const app = express(feathers());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
 app.configure(express.rest());
 app.configure(socketio());
-app.use('/messages', new MessageService());
+
+app.use('/uploads', blobService({Model: blobStorage}));
+
 app.use(express.errorHandler());
 
-app.on('connection', connection =>
-  app.channel('everybody').join(connection)
-);
-
-app.publish(data => app.channel('everybody'));
-
-app.listen(3030).on('listening', () =>
-  console.log('Feathers server listening on localhost:3030')
-);
-
-app.service('messages').create({
-  text: 'Hello world from the server'
+app.listen(3030, function(){
+  console.log('Feathers app started at localhost:3030')
 });
