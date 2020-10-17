46cd1faf2024f99b7d01012d4d8097aabf42d8f1
home No To ri <soy_solamente@hotmail.com>
2020-10-17 16:28:34 +0300

    add services

--- service/server.js
+++ service/server.js
@@ -1,23 +1,52 @@
 const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
-const socketio = require('@feathersjs/socketio');
-
+const cors = require('cors')
 const blobService = require('feathers-blob');
-
 const fs = require('fs-blob-store');
 const blobStorage = fs(__dirname + '/uploads/pictures');
-
 const app = express(feathers());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+
+
+class HelloService {
+
+    constructor() {
+        this.names = []
+    }
+
+    create(data, params) {
+        const name = data.name
+        this.names.push(name)
+        return Promise.resolve(this.helloTo(name))
+    }
+
+    find(params) {
+        return Promise.resolve(this.names.map(this.helloTo))
+    }
+
+    helloTo(name) {
+        return `Hello, ${name}!`
+    }
+
+}
+//TODO CHECK ENTITY ERROR
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true}));
 app.configure(express.rest());
-app.configure(socketio());
+app.use(cors(corsOptions))
 
 app.use('/uploads', blobService({Model: blobStorage}));
-
+app.use('/ping', new HelloService);
 app.use(express.errorHandler());
 
-app.listen(3030, function(){
-  console.log('Feathers app started at localhost:3030')
+app.listen(8000, function () {
+    console.log('Feathers app started at localhost:8000')
 });
+
+
+
